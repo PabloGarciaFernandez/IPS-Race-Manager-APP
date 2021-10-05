28d1540e596d2fe2d6b714e1c878778aa2876520
Juan <uo277963@uniovi.es>
2021-10-05 12:08:06 +0200

    estructura logica de negocio

---
+++ src/main/java/ipsTeamwork/business/atleta/Atleta.java
@@ -0,0 +1,5 @@
+package ipsTeamwork.business.atleta;
+
+public class Atleta {
+
+}
---
+++ src/main/java/ipsTeamwork/business/carrera/Carrera.java
@@ -0,0 +1,15 @@
+package ipsTeamwork.business.carrera;
+
+import java.util.Date;
+
+public class Carrera {
+
+	String nombre;
+	TipoCarrera tipo;
+	Date fecha;
+	double distancia;
+	float cuota;
+	Date fechaFin;
+	
+	int plazasDisp;
+}
