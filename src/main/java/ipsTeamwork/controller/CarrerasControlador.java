846ce85af478757bad5fffd3306f01b1866d1d71
Sergio Arroni <60364331+SergioArroni@users.noreply.github.com>
2021-10-10 23:49:10 +0200

    Update CarrerasControlador.java
    Esto sobraba :)

--- src/main/java/ipsTeamwork/controller/CarrerasControlador.java
+++ src/main/java/ipsTeamwork/controller/CarrerasControlador.java
@@ -2,38 +2,12 @@
  * 
  */
 package ipsTeamwork.controller;
-
-import java.util.ArrayList;
-
-import javax.swing.table.DefaultTableModel;
-
 import ipsTeamwork.model.carrera.CarreraDto;
-import ipsTeamwork.view.MainWindow;
 
 /**
  * @author Sergio Arroni Clase que aplica la logia de las carreras
  */
 public class CarrerasControlador {
-
-	public void VerCarreras() {
-
-		MainWindow mn = new MainWindow();
-		DefaultTableModel tb = (DefaultTableModel) mn.getTbVerCarreras().getModel();
-
-		GestorDB db = new GestorDB();
-
-		ArrayList<CarreraDto> carreras = db.selectCarrera();
-
-		for (CarreraDto carreraDto : carreras) {
-
-			String[] carrerasTabla = { carreraDto.getId(), carreraDto.getTipo(),
-					String.valueOf(carreraDto.getPlazasDisp()) };
-
-//			tb.addRow(carrerasTabla);
-
-			System.out.println(carreraDto.toString());
-
-		}
 
 	}
 
